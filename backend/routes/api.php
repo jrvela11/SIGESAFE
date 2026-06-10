781107926ad42b5eaaff44fb9a556649d7c7737a
Dante Delgado <smithdelgado111@gmail.com>
2026-06-09 23:31:17 -0500

    fix duplicado de rutas en api.php removidas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,11 +22,4 @@ Route::apiResource('users', App\Http\Controllers\UserController::class);
 
 Route::apiResource('sales', App\Http\Controllers\SaleController::class)->except('update');
 
-
-Route::apiResource('suppliers', App\Http\Controllers\SupplierController::class)->except('destroy');
-
-
-Route::apiResource('suppliers', App\Http\Controllers\SupplierController::class)->except('destroy');
-
-
 Route::apiResource('suppliers', App\Http\Controllers\SupplierController::class);
