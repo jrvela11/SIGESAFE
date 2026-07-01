01a9047fe0d02e7ae0d4e3bb32ef7b2a905b00a2
Dante Delgado <smithdelgado111@gmail.com>
2026-07-01 15:04:56 -0500

    implementacion de proteccion de rutas mediante jwt

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+
+    
+});
 
 Route::get('/reports/export-pdf', [ReportController::class, 'exportPdf']);
+
+
 Route::get('/customers/verify-document', [CustomerController::class, 'verifyDocument']);
 Route::apiResource('customers', App\Http\Controllers\CustomerController::class);
 
